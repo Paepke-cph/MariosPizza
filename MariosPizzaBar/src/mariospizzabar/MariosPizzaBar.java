9929b021eb641733326ae5b45fe4266cb094401d
Benjamin <cph-bp133@cphbusiness.dk>
2019-03-21 15:20:56 +0100

    Test

--- MariosPizzaBar/src/mariospizzabar/MariosPizzaBar.java
+++ MariosPizzaBar/src/mariospizzabar/MariosPizzaBar.java
@@ -5,6 +5,7 @@ public class MariosPizzaBar {
 
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Benjamin TEST");
         System.out.println("Tobias");
     }
 
