a1fa455d85a60cfa9ec9bd59511dc44fed3c893e
Benjamin <cph-bp133@cphbusiness.dk>
2019-03-25 20:20:43 +0100

    Added more tests for orders and some for SystemMenu

--- MariosPizzaBar/Test Packages/core/OrderTest.java
+++ MariosPizzaBar/Test Packages/core/OrderTest.java
@@ -1,5 +1,6 @@
 package core;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -8,11 +9,14 @@ public class OrderTest {
 
     private Order order;
     private Customer customer;
+    private LocalTime pickUpTime;
     public OrderTest() {
+        pickUpTime = LocalTime.of(18, 30);
         customer = new Customer("Benjamin","11 22 33 44");
         ArrayList<Pizza> pizza = new ArrayList<>();
         pizza.add(new Pizza(1, "Amerikaner", "Pepperoni", 59));
-        order = new Order(1, pizza, customer);
+        
+        order = new Order(1, pizza, customer, pickUpTime);
     }
 
     @Test
@@ -28,5 +32,10 @@ public class OrderTest {
      @Test
      public void testGetCustomer() {
          assertEquals(customer, order.getCustomer());
+     }
+     
+     @Test
+     public void testGetPickUpTime() {
+         assertEquals(pickUpTime, order.getPickUpTime());
      }
 }
---
+++ MariosPizzaBar/Test Packages/core/SystemMenuTest.java
@@ -0,0 +1,33 @@
+package core;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SystemMenuTest {
+
+    SystemMenu menu;
+    public SystemMenuTest() {
+         menu = SystemMenu.MAIN_MENU;
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("Main Menu", menu.getName());
+    }
+
+    /**
+     * NOTE: Det her er ikke en super god test, da indholdet af test
+     * bliver generet ved run-time og derfor ikke er lige til at definerer
+     */
+    @Test
+    public void testGetText() {
+        assertTrue(menu.getText().contains("This is the main menu"));
+        assertFalse(menu.getText().contains("This is the pizza menu"));
+    }
+
+    @Test
+    public void testGetMenu() {
+        assertEquals(SystemMenu.PIZZA_MENU, menu.getMenu(0));
+        assertEquals(SystemMenu.MAIN_MENU, menu.getMenu(99));
+    }
+}
