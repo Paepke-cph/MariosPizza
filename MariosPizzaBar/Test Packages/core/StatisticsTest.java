15ece54749653587118ed203af9fe2f3e51f8808
Benjamin <cph-bp133@cphbusiness.dk>
2019-03-27 09:07:59 +0100

    final

--- MariosPizzaBar/Test Packages/core/StatisticsTest.java
+++ MariosPizzaBar/Test Packages/core/StatisticsTest.java
@@ -1,7 +1,6 @@
 package core;
 
 import Storage.MockStorage;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -10,9 +9,7 @@ public class StatisticsTest {
 
     private final double val1 = 114.0, val2 = 175.0, val3 = 57.0;
     
-    public StatisticsTest() {
-        
-    }
+    public StatisticsTest() {}
 
     @Test
     public void testCalculateTurnover() {
@@ -31,48 +28,7 @@ public class StatisticsTest {
                 startDate, endDate, (val1+val2+val3));
         assertEquals(exp,output);
     }
-
-    @Test
-    public void testTurnoverForDuration() {
-        MockStorage mock = new MockStorage();
-        ArrayList<String> fileLines = new ArrayList<>();
-        fileLines.add("88 "+val1+" 2019-03-26");
-        fileLines.add("89 "+val2+" 2019-03-26");
-        fileLines.add("90 "+val3+" 2019-03-26");
-
-        mock.setFileLines(fileLines);
-        Statistics stats = new Statistics(mock);
-        
-        int endDate = 20190326;
-        int startDate = endDate-7;
-        String output = stats.turnoverForDuration(0, 0, 1);
-        String exp = String.format("Turnover from %d to %d = %.2f kr%n",
-                startDate, endDate, (val1+val2+val3));
-        assertEquals(exp,output);
-    }
-
-    @Test
-    public void testTurnoverAllTime() {
-        MockStorage mock = new MockStorage();
-        ArrayList<String> fileLines = new ArrayList<>();
-        fileLines.add("88 "+val1+" 2019-03-26");
-        fileLines.add("89 "+val2+" 2019-03-26");
-        fileLines.add("90 "+val3+" 2019-03-26");
-
-        mock.setFileLines(fileLines);
-        Statistics stats = new Statistics(mock);
-        
-        int endDate = 20190326;
-        int startDate = Integer.parseInt(LocalDate.now()
-                .withYear(2017)
-                .toString()
-                .replace("-", ""));
-        String output = stats.turnoverAllTime();
-        String exp = String.format("Turnover from %d to %d = %.2f kr%n",
-                startDate, endDate, (val1+val2+val3));
-        assertEquals(exp,output);
-    }
-
+    
     @Test
     public void testCalculatePopularPizza() {
         MockStorage mock = new MockStorage();
@@ -87,52 +43,6 @@ public class StatisticsTest {
         Statistics stats = new Statistics(mock);
         
         int startDate = 20190326, endDate = 20190326;
-        String output = stats.calculatePopularPizza(startDate, endDate);
-        String exp = String.format("Most popular pizza from %d to %d = number %d%n",
-                startDate, endDate, 1);
-        assertEquals(exp,output);
-    }
-
-    @Test
-    public void testPopularPizzaForDuration() {
-        MockStorage mock = new MockStorage();
-        ArrayList<String> fileLines = new ArrayList<>();
-        fileLines.add("1 2019-03-26");
-        fileLines.add("1 2019-03-26");
-        fileLines.add("1 2019-03-26");
-        fileLines.add("2 2019-03-26");
-        fileLines.add("3 2019-03-26");
-
-        mock.setFileLines(fileLines);
-        Statistics stats = new Statistics(mock);
-        
-        int endDate = 20190326;
-        int startDate = endDate-7;
-        
-        String output = stats.popularPizzaForDuration(0, 0, 1);
-        String exp = String.format("Most popular pizza from %d to %d = number %d%n",
-                startDate, endDate, 1);
-        assertEquals(exp,output);
-    }
-
-    @Test
-    public void testPopularPizzaAllTime() {
-        MockStorage mock = new MockStorage();
-        ArrayList<String> fileLines = new ArrayList<>();
-        fileLines.add("1 2019-03-26");
-        fileLines.add("1 2019-03-26");
-        fileLines.add("1 2019-03-26");
-        fileLines.add("2 2019-03-26");
-        fileLines.add("3 2019-03-26");
-
-        mock.setFileLines(fileLines);
-        Statistics stats = new Statistics(mock);
-        
-        int endDate = 20190326;
-        int startDate = Integer.parseInt(LocalDate.now()
-                .withYear(2017)
-                .toString()
-                .replace("-", ""));
         String output = stats.calculatePopularPizza(startDate, endDate);
         String exp = String.format("Most popular pizza from %d to %d = number %d%n",
                 startDate, endDate, 1);
